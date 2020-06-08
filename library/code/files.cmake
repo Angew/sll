259e47b383668dc2eaa756fcb9480902e828496a
Petr Kmoch <petr.kmoch@gmail.com>
2020-06-08 15:30:41 +0200

    WIP on initial infrastructure

---
+++ library/code/files.cmake
@@ -0,0 +1,11 @@
+# Copyright Petr Kmoch 2020
+# Distributed under the Boost Software License, Version 1.0
+# (See accompanying LICENSE.txt or copy at https://www.boost.org/LICENSE_1_0.txt)
+
+set(files_public_headers
+	include/sll/memory.hpp
+)
+
+set(files_protected_headers
+	include/sll/impl/config.hpp
+)
